3f9f23083f8976522a4e47afc9d7d5076ff744f6
Harshaiag <hsm23@student.le.ac.uk>
2023-05-25 02:58:34 +0100

    Commit

--- assets/js/introanime.js
+++ assets/js/introanime.js
@@ -1,22 +1,26 @@
 let a = 1;
-let aChange = 0.1;
+let aChange = 0.001;
 let canvas;
+let values = [];
 
 function setup() {
-    canvas = createCanvas(windowWidth, windowHeight); // Adjust the height to fit the banner
-    canvas.parent('p5Canvas'); // Place the canvas in the banner element
-    background(0); // Set the background to black
+    canvas = createCanvas(windowWidth, windowHeight);
+    canvas.parent('p5Canvas');
+    background(0);
     canvas.style('z-index', '-2');
     canvas.position(0, 0);
     pixelDensity(1);
 
+    calculateValues();
 }
 
 function draw() {
     loadPixels();
+
     for (let x = 0; x < width; x++) {
         for (let y = 0; y < height; y++) {
-            let val = tan((x - width / 2) ** 2 + (y - height / 2) ** 2);
+            let val = values[x][y];
+
             if (abs(val - a) < 0.01) {
                 let pix = (x + y * width) * 4;
                 pixels[pix + 0] = 0;
@@ -30,8 +34,21 @@ function draw() {
     updatePixels();
 
     a += aChange;
+
     if (a > 0.05 * PI || a < -0.05 * PI) {
         a = 0;
+        calculateValues(); // Recalculate values when a reaches the limit
     }
 }
-			
+
+function calculateValues() {
+    values = [];
+
+    for (let x = 0; x < width; x++) {
+        values[x] = [];
+
+        for (let y = 0; y < height; y++) {
+            values[x][y] = tan((x - width / 2) ** 2 + (y - height / 2) ** 2);
+        }
+    }
+}
